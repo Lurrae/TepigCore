30de8b7d7e50992c190031259630579b1b54db8f
Tepig125 <ar.murphy@live.com>
2023-05-03 19:15:29 -0500

    Update ModWhipProj with numerous improvements

--- Base/ModdedProjectile/ModWhipProj.cs
+++ Base/ModdedProjectile/ModWhipProj.cs
@@ -7,47 +7,45 @@ using Terraria.Audio;
 using Terraria.GameContent;
 using Terraria.ID;
 using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
 
 namespace TepigCore.Base.ModdedProjectile
 {
-	public abstract class ModWhipProj : ModProjectile
+	public abstract class ModWhipProj<TagDebuff> : ModProjectile where TagDebuff : ModBuff
 	{
-		// Note: This code was adapted from JohnSnail's ExampleWhipProjectile, though some things had to be changed
+		public virtual Texture2D FishingLineOverrideTexture => null;
+		public virtual Vector2? OverrideTextureOrigin => null;
+
 		public override void SetStaticDefaults()
 		{
 			ProjectileID.Sets.IsAWhip[Type] = true;
 		}
 
+		/// <summary>
+		/// Anything that needs to be set per-whip can be done here, mainly Projectile.WhipSettings values
+		/// </summary>
 		public virtual void WhipSetDefaults()
 		{
-			// Anything that needs to be set per-whip can be done here, like ownerHitCheck or extraUpdates
 		}
+
+		public float Falloff = 0.3f; // By what percentage does damage get reduced by each time the whip hits something? 0% means no damage falloff, 100% means only the first enemy hit takes any damage
+		public Vector2 SpriteSize = new(22, 28); // The size of one frame of the whip's size
+		public int ExtraDustID = -1; // If this whip summons extra dust particles, it should use this ID. If this is -1, no extra dust is spawned
+		public float DustVelMult = 2; // If this whip summons extra dust particles, how fast should they travel?
+		public bool IsFullbright = false; // Should the whole whip be drawn without taking brightness into account?
+		public bool FullbrightLine = false; // Should only the line be drawn without brightness?
+		public bool HasLine = true; // Whether or not the whip draws a line behind its sprite
+		public Color LineColor = Color.White; // If the whip should draw a line, this color is used for it
+		public float ScaleMultiplier = 1; // How large should the whip's sprite be?
+		public bool UseRandomSegments = false; // Should the middle segments of the whips be selected at random (true) or based on the order in the sprite sheet (false)?
+		public bool DisableTipScaling = false; // Should the tip of the whip scale up as the whip flies out?
 
 		public override void SetDefaults()
 		{
+			Projectile.DefaultToWhip();
+
 			WhipSetDefaults();
-
-			Projectile.width = 22;
-			Projectile.height = 28;
-			Projectile.friendly = true;
-			Projectile.penetrate = -1;
-			Projectile.tileCollide = false;
-			Projectile.usesLocalNPCImmunity = true;
-			Projectile.localNPCHitCooldown = -1;
-			Projectile.DamageType = DamageClass.Summon;
 		}
-
-		public abstract int TagDebuff { get; } // What debuff causes the tag damage
-		public virtual int ExtraDebuff() { return 0; } // If this whip has an extra debuff, it goes here. Defaults to 0 for no extra debuff
-		public abstract float Falloff { get; } // By what percentage does damage get reduced by each time the whip hits something?
-		public abstract bool ExtraDust { get; } // Whether or not the whip has dust as it travels, like Kaleidoscope
-		public virtual int ExtraDustID() { return 0; } // If this whip summons extra dust particles, it should use this ID
-		public virtual float DustVelMult() { return 2f; } // If this whip summons extra dust particles, how fast should they travel?
-		public abstract bool Fullbright { get; } // Should the whip be drawn without taking brightness into account?
-		public abstract bool HasLine { get; } // Whether or not the whip draws a line behind its sprite
-		public virtual Color LineColor() { return Color.White; } // If the whip should draw a line, this color is used for it
-		public virtual float ScaleMult() { return 1f; } // How large should the whip's sprite be?
-		public abstract bool RandomMidSegments { get; } // Should the middle segments of the whips be selected at random (true) or based on the order in the sprite sheet (false)?
 
 		private float Timer
 		{
@@ -55,36 +53,20 @@ namespace TepigCore.Base.ModdedProjectile
 			set => Projectile.ai[0] = value;
 		}
 
-		public override void AI()
+		/// <summary>
+		/// Use this to add any extra behavior to a whip
+		/// </summary>
+		public virtual void WhipAI()
 		{
-			Player owner = Main.player[Projectile.owner];
-			Projectile.rotation = Projectile.velocity.ToRotation() + MathHelper.PiOver2;
+		}
 
-			Projectile.Center = Main.GetPlayerArmPosition(Projectile) + Projectile.velocity * Timer;
-			Projectile.spriteDirection = Projectile.velocity.X >= 0f ? 1 : -1;
-			Timer++;
-
-			Projectile.GetWhipSettings(Projectile, out float timeToFlyOut, out int _, out float _);
-
-			if (Timer >= timeToFlyOut || owner.itemAnimation <= 0)
-			{
-				Projectile.Kill();
-				return;
-			}
-			owner.heldProj = Projectile.whoAmI;
-			owner.itemAnimation = owner.itemAnimationMax - (int)(Timer / Projectile.MaxUpdates);
-			owner.itemTime = owner.itemAnimation;
-
-			if (Timer == (int)(timeToFlyOut / 2))
-			{
-				List<Vector2> points = Projectile.WhipPointsForCollision;
-				Projectile.FillWhipControlPoints(Projectile, points);
-				SoundEngine.PlaySound(SoundID.Item153, points[^1]);
-			}
-
+		public sealed override void AI()
+		{
 			// Draw some extra dust as the whip travels
-			if (ExtraDust)
+			if (ExtraDustID > -1)
 			{
+				Projectile.GetWhipSettings(Projectile, out float timeToFlyOut, out int _, out float _);
+				
 				float t = Timer / timeToFlyOut;
 				if (Utils.GetLerpValue(0.1f, 0.7f, t, true) * Utils.GetLerpValue(0.9f, 0.7f, t, true) > 0.1f)
 				{
@@ -94,31 +76,43 @@ namespace TepigCore.Base.ModdedProjectile
 
 					Vector2 vel = Projectile.WhipPointsForCollision[^2].DirectionTo(Projectile.WhipPointsForCollision[^1]).SafeNormalize(Vector2.Zero);
 
-					Dust whipDust = Dust.NewDustDirect(beltTip.TopLeft(), beltTip.Width, beltTip.Height, ExtraDustID());
-					whipDust.velocity = vel * DustVelMult();
+					Dust whipDust = Dust.NewDustDirect(beltTip.TopLeft(), beltTip.Width, beltTip.Height, ExtraDustID);
+					whipDust.velocity = vel * DustVelMult;
 				}
 			}
+
+			WhipAI();
+		}
+
+		/// <summary>
+		/// Additional on-hit whip effects, like a secondary debuff or a visual effect, can be applied here
+		/// </summary>
+		/// <param name="target"></param>
+		/// <param name="hit"></param>
+		/// <param name="damageDone"></param>
+		public virtual void OnHitExtras(NPC target, NPC.HitInfo hit, int damageDone)
+		{
 		}
 
 		// This triggers the tag damage on enemies and sets the minion target
-		public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
+		public sealed override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
 		{
-			target.AddBuff(TagDebuff, 240);
-			if (ExtraDebuff() != 0)
-				target.AddBuff(ExtraDebuff(), 240);
+			target.AddBuff(BuffType<TagDebuff>(), 240);
 
 			Main.player[Projectile.owner].MinionAttackTargetNPC = target.whoAmI;
 
 			// Reducing damage per enemy hit based on falloff value
 			Projectile.damage = (int)(Projectile.damage * (1f - Falloff));
+
+			OnHitExtras(target, hit, damageDone);
 		}
 
 		// TODO: Figure out how to use the scaleMult parameter
-		private static void DrawLine(List<Vector2> list, bool fullBright, Color lineColor, float scaleMult)
+		private void DrawLine(List<Vector2> list, bool fullBright, Color lineColor, float scaleMult)
 		{
-			Texture2D texture = TextureAssets.FishingLine.Value;
+			Texture2D texture = FishingLineOverrideTexture ?? TextureAssets.FishingLine.Value;
 			Rectangle frame = texture.Frame();
-			Vector2 origin = new((frame.Width - 8) / 2, 2);
+			Vector2 origin = OverrideTextureOrigin ?? new((frame.Width - 8) / 2, 2);
 
 			Vector2 pos = list[0];
 			for (int i = 0; i < list.Count - 2; i++)
@@ -150,17 +144,17 @@ namespace TepigCore.Base.ModdedProjectile
 			Projectile.FillWhipControlPoints(Projectile, list);
 
 			if (HasLine)
-				DrawLine(list, Fullbright, LineColor(), ScaleMult());
+				DrawLine(list, IsFullbright || FullbrightLine, LineColor, ScaleMultiplier);
 			SpriteEffects flip = Projectile.spriteDirection < 0 ? SpriteEffects.None : SpriteEffects.FlipHorizontally;
 
-			Main.instance.LoadProjectile(Projectile.type);
-			Texture2D texture = TextureAssets.Projectile[Projectile.type].Value;
+			Texture2D texture = (Texture2D)Request<Texture2D>(Texture);
 
 			Vector2 pos = list[0];
+			int j = 0;
 
 			for (int i = 0; i < list.Count - 1; i++)
 			{
-				Rectangle frame = new(0, 0, 22, 28);
+				Rectangle frame = new(0, 0, (int)SpriteSize.X, (int)SpriteSize.Y);
 				Vector2 origin = new(5, 8);
 				float scale = 1;
 
@@ -168,22 +162,27 @@ namespace TepigCore.Base.ModdedProjectile
 				// frame.Y marks the start of the area to grab, with frame.Height being the height of the area
 				if (i == list.Count - 2)
 				{
-					frame.Y = 112;
-					frame.Height = 28;
+					frame.Height = (int)SpriteSize.Y;
+					frame.Y = frame.Height * 4;
 
-					Projectile.GetWhipSettings(Projectile, out float timeToFlyOut, out int _, out float _);
+					Projectile.GetWhipSettings(Projectile, out float timeToFlyOut, out _, out _);
 					float t = Timer / timeToFlyOut;
-					scale = MathHelper.Lerp(0.5f, 1.5f, Utils.GetLerpValue(0.1f, 0.7f, t, true) * Utils.GetLerpValue(0.9f, 0.7f, t, true));
+					if (!DisableTipScaling)
+						scale = MathHelper.Lerp(0.5f, 1.5f, Utils.GetLerpValue(0.1f, 0.7f, t, true) * Utils.GetLerpValue(0.9f, 0.7f, t, true));
 				}
 				else if (i > 0)
 				{
-					int j = 0;
-					int[] MidSprites = new int[] { 28, 56, 84 };
+					int[] MidSprites = new int[] { frame.Height, frame.Height * 2, frame.Height * 3 };
 
-					if (i % 8 == 0)
+					Projectile.GetWhipSettings(Projectile, out _, out int segments, out _);
+					if (i % MathF.Floor(segments / 3) == 0)
+					{
 						j++;
+						if (j > 2)
+							j = 0;
+					}
 
-					if (RandomMidSegments)
+					if (UseRandomSegments)
 					{
 						if (!HasRandomizedMid)
 						{
@@ -200,7 +199,7 @@ namespace TepigCore.Base.ModdedProjectile
 						MidSprite = MidSprites[Math.Max(j, 2)];
 					}
 					frame.Y = MidSprite;
-					frame.Height = 28;
+					frame.Height = (int)SpriteSize.Y;
 				}
 
 				Vector2 element = list[i];
@@ -209,18 +208,16 @@ namespace TepigCore.Base.ModdedProjectile
 				float rotation = diff.ToRotation() - MathHelper.PiOver2;
 				Color color = Color.White;
 
-				if (!Fullbright)
-				{
+				if (!IsFullbright)
 					color = Lighting.GetColor(element.ToTileCoordinates(), Color.White);
-				}
 
-				Main.EntitySpriteDraw(texture, pos - Main.screenPosition, frame, color, rotation, origin, scale * ScaleMult(), flip, 0);
+				Main.EntitySpriteDraw(texture, pos - Main.screenPosition, frame, color, rotation, origin, scale * ScaleMultiplier, flip, 0);
 
 				pos += diff;
 			}
 
 			// Doing this check here so that it goes through all of the segments and randomizes them first
-			if (!HasRandomizedMid && RandomMidSegments)
+			if (!HasRandomizedMid && UseRandomSegments)
 			{
 				HasRandomizedMid = true;
 			}
